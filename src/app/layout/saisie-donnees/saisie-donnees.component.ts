b6e9efaefd00f695ed6f3c8563cd0e8f3b2868fa
MNassim26 <nassim.mekki8@gmail.com>
2019-03-29 20:10:33 +0100

    saisie donnees

--- src/app/layout/saisie-donnees/saisie-donnees.component.ts
+++ src/app/layout/saisie-donnees/saisie-donnees.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-saisie-donnees',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saisie-donnees.component.css']
 })
 export class SaisieDonneesComponent implements OnInit {
-
+  role : String ="Quartet";
   constructor() { }
 
   ngOnInit() {
+    $(".RH").hide();
+    $(".formations").hide();
+    $(".projets").hide();
+    $(".communication").hide();
+    $(".devCommercial").hide();
+
+    if (this.role == 'Quartet'){
+      $(".RH").fadeIn(0);
+      $(".formations").fadeIn(0);
+      $(".projets").fadeIn(0);
+      $(".communication").fadeIn(0);
+      $(".devCommercial").fadeIn(0);
+    }
+    else if (this.role == 'RH'){
+      $(".RH").fadeIn(0);
+    }
+    else if (this.role == "Formations"){
+      $(".formations").fadeIn(0);
+    }
+    else if (this.role == "RH & Formations"){
+      $(".RH").fadeIn(0);
+      $(".formations").fadeIn(0);
+    }
+    else if(this.role == "Projets"){
+      $(".projets").fadeIn(0);
+    }
+    else if(this.role == "Communication"){
+      $(".communication").fadeIn(0);
+    }
+    else if(this.role == "Devloppement Commercial"){
+      $(".devCommercial").fadeIn(0);
+    }
   }
 
 }
