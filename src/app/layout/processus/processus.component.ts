dced92e8d2b855ca3baffc0e5363279959c12725
MNassim26 <nassim.mekki8@gmail.com>
2019-03-29 02:11:33 +0100

    processus done

--- src/app/layout/processus/processus.component.ts
+++ src/app/layout/processus/processus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-processus',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./processus.component.css']
 })
 export class ProcessusComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    $(".RH").hide();
+    $(".formations").hide();
+    $(".projets").hide();
+    $(".communication").hide();
+    $(".devCommercial").hide();
   }
 
+  visibilityRH(){
+    $('.content-img').fadeOut(300);
+    $('.formations').fadeOut(100);
+    $('.projets').fadeOut(100);
+    $('.communication').fadeOut(100);
+    $('.devCommercial').fadeOut(100);
+    $('.RH').fadeIn(1000);
+  }
+
+  visibilityFormations(){
+    $('.content-img').fadeOut(300);
+    $('.RH').fadeOut(100);
+    $('.projets').fadeOut(100);
+    $('.communication').fadeOut(100);
+    $('.devCommercial').fadeOut(100);
+    $('.formations').fadeIn(1000);
+  }
+
+  visibilityProjets(){
+    $('.content-img').fadeOut(300);
+    $('.RH').fadeOut(100);
+    $('.formations').fadeOut(100);
+    $('.communication').fadeOut(100);
+    $('.devCommercial').fadeOut(100);
+    $('.projets').fadeIn(1000);
+  }
+
+  visibilityCommunication(){
+    $('.content-img').fadeOut(300);
+    $('.RH').fadeOut(100);
+    $('.formations').fadeOut(100);
+    $('.projets').fadeOut(100);
+    $('.devCommercial').fadeOut(100);
+    $('.communication').fadeIn(1000);
+  }
+
+  visibilityDevCommercial(){
+    $('.content-img').fadeOut(300);
+    $('.RH').fadeOut(100);
+    $('.formations').fadeOut(100);
+    $('.projets').fadeOut(100);
+    $('.communication').fadeOut(100);
+    $('.devCommercial').fadeIn(1000);
+  }
 }
